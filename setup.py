fa2ffce0a1fe9dd6c60c0bbed1cc6388bfb194f3
Guillaume Gricourt <guipagui@gmail.com>
2022-01-19 12:23:52 +0100

    chore(github): add packaging with pip

--- setup.py
+++ setup.py
@@ -1,32 +1,45 @@
-# coding: utf-8
+import glob
 import os
+import setuptools
 import yaml
 
-from setuptools import setup
 
+# Version
+version = ''
+fversion = glob.glob(
+    '**/_version.py',
+    recursive=True
+)
+fversion = fversion[0]
+with open(fversion) as fid:
+    lines = fid.read().splitlines()
+    version = lines[0].split("=")[-1].strip().replace('"', '')
 
 # App name - dependencies
 env = {}
 with open('environment.yml') as fid:
     env = yaml.safe_load(fid)
 name = env['name']
-install_requires = env['dependencies']
+install_requires = []
+for package in env['dependencies']:
+    if isinstance(package, dict):
+        package = package.get('pip', [])
+        install_requires += package
+    else:
+        install_requires.append(package)
+description = 'Perform FBA analysis to find best combination of genes \
+    to delete to maximize a target'
 
-# Version
-version = ''
-with open(os.path.join(name, '_version.py')) as fid:
-    lines = fid.read().splitlines()
-    version = lines[0].split("=")[-1].strip().replace('"', '')
-
-setup(
+setuptools.setup(
     name=name,
     version=version,
     author=['guillaume-gricourt'],
     author_email=['guipagui@gmail.com'],
-    description='',
+    description=description,
     long_description_content_type='text/markdown',
     url='https://github.com/brsynth/rpFbaAnalysis',
-    packages=[name],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     include_package_data=True,
     license='MIT',
     classifiers=[
