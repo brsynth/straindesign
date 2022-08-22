5cc2ee7ee88057ab07cd966831f22e1ba38b0d89
Guillaume Gricourt <guipagui@gmail.com>
2022-08-22 12:14:50 +0200

    tests(galaxy): add test for galaxy wrapper

--- tests/test_software.py
+++ tests/test_software.py
@@ -106,3 +106,35 @@ class Test_software(Main_test):
                 assert dialect.delimiter == "\t"
 
         os.remove(fd.name)
+
+    def test_software_galaxy(self):
+        # Be careful: can not test gene annotation into
+        # worflows running simultaneously
+        with tempfile.NamedTemporaryFile(delete=False) as fd:
+            args = ["python", "-m", __app_name__]
+            args += ["--input-model-file", self.model_ecoli_gz]
+            args += ["--input-pathway-file", self.pathway_butanol]
+            args += ["--biomass-rxn-id", "BIOMASS_Ec_iAF1260_core_59p81M"]
+            args += ["--target-rxn-id", "EX_1btol_e"]
+            args += ["--substrate-rxn-id", "EX_glc__D_e"]
+            args += ["--output-file-tsv", fd.name]
+            args += ["--strategy", "ko"]
+            args += ["--max-knockouts", "3"]
+            args += ["--input-medium-file", self.medium_butanol_tsv]
+            args += ["--thread", "1"]
+
+            ret = Test_software.launch(args)
+            if ret.returncode > 0:
+                print(ret.stderr)
+                print(ret.stdout)
+                sys.exit(1)
+            lines = []
+            with open(fd.name) as fid:
+                lines = fid.read().splitlines()
+            self.assertGreater(len(lines), 0)
+
+            # Check delimiter
+            with open(fd.name) as fid:
+                dialect = csv.Sniffer().sniff(fid.readline())
+                assert dialect.delimiter == "\t"
+        os.remove(fd.name)
